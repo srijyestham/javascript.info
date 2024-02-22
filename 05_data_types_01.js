05a20b83bad388961878188a7aed2b5e65e2fd7c
SriJyestham <sriindram18@gmail.com>
2024-02-22 05:39:16 -0500

    Data Types

---
+++ 05_data_types_01.js
@@ -0,0 +1,29 @@
+let message = "hello";
+alert(message);
+
+message = 12345678910;
+alert(message);
+
+let n = 123;
+alert(n);
+
+n = 12.34;
+alert(n);
+
+alert(1 / 0);
+
+alert(Infinity);
+
+alert(-Infinity);
+
+alert(NaN);
+
+alert("not a number" / 2); // NaN output
+
+alert(NaN + 1); //NaN
+
+alert(3 * NaN); //NaN
+
+alert( "not a number" / 2 - 1); //NaN
+
+alert ( NaN ** 0 ); // 1
