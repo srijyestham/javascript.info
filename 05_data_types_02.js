82b271b750c8f7e130cd241e07903b9b1f09adc7
SriJyestham <sriindram18@gmail.com>
2024-02-24 04:44:06 -0500

    data types 04

---
+++ 05_data_types_02.js
@@ -0,0 +1,42 @@
+
+
+let str = "Hello";
+alert(str);
+
+let str2 = 'Hello';
+alert(str2);
+
+let str3 = `hello ${str2}`;
+alert(str3);
+
+let name = "John";
+alert(`Hello, ${name}`);
+
+alert(`the result is ${1 + 2}`);
+
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+alert(nameFieldChecked);
+alert(ageFieldChecked);
+
+let isGreater = 4 > 1;
+
+alert(isGreater);
+
+let age = null;
+
+alert(age);
+alert(null);
+
+let age2;
+
+alert(age2);
+
+alert(undefined);
+
+let age3 = 100;
+
+age3 = undefined;
+
+alert(age3);
---
+++ 05_data_types_03.js
@@ -0,0 +1,11 @@
+
+
+alert(typeof(undefined));
+alert(typeof(0));
+alert(typeof(10n));
+alert(typeof(true));
+alert(typeof("foo"));
+alert(typeof(Symbol("id")));
+alert(typeof(Math));
+alert(typeof(null));
+alert(typeof(alert));
