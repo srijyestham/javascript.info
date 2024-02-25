e3f1f1716ee490f4883347874f4d38f9179b1d7c
SriJyestham <sriindram18@gmail.com>
2024-02-25 04:32:59 -0500

    interaction

---
+++ 06_interaction_01.js
@@ -0,0 +1,9 @@
+
+let age = prompt("How old are you ?", 100);
+
+alert(`You are ${age} years old!`);
+
+
+let isBoss = confirm("Are you the boss?");
+
+alert(isBoss);
---
+++ 06_interaction_02.js
@@ -0,0 +1,4 @@
+
+let name = prompt("What is your name ?", '');
+
+alert(`Hello, ${name}!`);
